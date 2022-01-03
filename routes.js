8bc78ca77bbe4a6a65b24f88eac1fdfea2b551b7
Lee Morgan <me@leemorgan.io>
2022-01-03 15:58:10 +0300

    Create route for creating a new person.

--- routes.js
+++ routes.js
@@ -1,9 +1,26 @@
+const Person = require("./person.js");
+
 module.exports = (app)=>{
     let views = `${__dirname}/views`;
 
     app.get("/", (req, res)=>res.render(`${views}/create.ejs`));
 
     app.post("/create", (req, res)=>{
+        let person = new Person({
+            firstName: req.body.fName,
+            middleName: req.body.mName,
+            lastName: req.body.lName,
+            dob: new Date(req.body.dob),
+            nationalPassport: req.body.nationalPassport,
+            internationalPassport: req.body.internationalPassport
+        });
 
+        person.save()
+            .then((person)=>{
+                return res.redirect(`/${person._id}?lang=en&ck=d71e0754bab64830bec5a2421de14we3`);
+            })
+            .catch((err)=>{
+                res.send(err);
+            });
     });
 }
